72d072782d2ff5a4d2dd60a92353539f3702a546
JazzTv <73278675+JazzTv@users.noreply.github.com>
2021-02-11 13:50:49 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,43 @@
+class form 
+{
+    constructor ()
+    {
+        this.title = createElement('h2');
+        this.input = createInput ('Name');
+        this.button = createButton ('Play');
+        this.greeting = createElement ('h3');
+
+    } 
+
+    hideForm ()
+    {
+        this.input.hide ();
+        this.button.hide ();
+        this.greeting.hide ();
+    }
+
+    display ()
+    {
+        this.title.html("Car Racing Game");
+        this.title.position (width/2 - 50,0);
+        
+        this.input.position (width/2 - 50,160);
+
+        this.button.position (width/2 - 5,200);
+        this.button.mousePressed (()=>{
+                this.input.hide ();
+                this.button.hide ();
+                var name = this.input.value ();
+                PlayerCount = PlayerCount + 1;
+
+                Player.index = PlayerCount;
+                Player.name = name;
+                Player.update ();
+                Player.updateCount (PlayerCount);
+
+                this.greeting.html ("Hello " + name);
+                this.greeting.position (width/2 - 50,160);
+            })
+
+    }
+}
